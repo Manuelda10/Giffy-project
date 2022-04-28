07f399a6bacf11ba80a69198222b5a3dca799ddf
Manuel Vallejos <manuel.vallejos@unmsm.edu.pe>
2022-04-27 23:04:40 -0500

    Faltan correcciones en el login y register

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './Modal.css'
+import 'boxicons'
 
 function Modal({children, onClose}) {
     return <div className='modal'>
         <div className='modal-content'>
-            <button className='close-btn' onClick={onClose}>X</button>
+            <button className='close-btn' onClick={onClose}><box-icon name='x' size='sm' animation='spin-hover'></box-icon></button>
             {children}
         </div>
     </div>
